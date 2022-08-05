6487549e62be2d87ea7351bd43b3acd300b19d15
Victor Manuel Islas Espinoza <islasvictorm@gmail.com>
2022-08-05 00:43:35 -0500

    added dynamo base

---
+++ app/InMemory/IdentityServer/Data/Models/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IdentityServer.Data.Models;
+
+public abstract class Item<T>
+{
+    public abstract string Pk
+    {
+        get;
+    }
+    public abstract string Sk
+    {
+        get;
+    }
+
+    public DynamoDbKeys Keys() => new DynamoDbKeys() { PK = this.Pk, SK = this.Sk };
+
+    public abstract T ToItem();
+}
+
+
+public class DynamoDbKeys
+{
+    public string PK
+    {
+        get; set;
+    }
+    public string SK
+    {
+        get; set;
+    }
+}
---
+++ app/InMemory/IdentityServer/Data/Models/Organization.cs
@@ -0,0 +1,29 @@
+﻿namespace IdentityServer.Data.Models;
+
+public class Organization : Item<Organization>
+{
+    public string Name
+    {
+        get; set;
+    }
+    public string Description
+    {
+        get; set;
+    }
+
+    public Organization(string name, string description = "")
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public override string Pk => $"ORG#${this.Name}";
+
+    public override string Sk => $"ORG#${this.Name}";
+
+
+    public override Organization ToItem()
+    {
+        return this;
+    }
+}
